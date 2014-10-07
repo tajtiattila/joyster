181bbdc1ef007f3a4529bd2a49a0989becf9c904
Attila Tajti <attila.tajti@gmail.com>
2014-10-07 18:46:17 +0200

    remove block constraints from parser altogether
    Input and output constraints should be checked in sort(). This is
    necessary to make blocks with optional inputs work.

--- block/parser/parser.go
+++ block/parser/parser.go
@@ -140,7 +140,7 @@ func (p *parser) parsesource() (input specSource) {
 	switch {
 	case p.r.ch() == '[':
 		lno := p.r.sourceline()
-		blk, _ := p.newstandaloneblk("", &blkconstraint{inpdisp: inpdef_required})
+		blk, _ := p.newstandaloneblk("", inpdef_required)
 		blk.oc = &outputconstraint{"block used as input source must have unnamed output", []string{""}}
 		input = &concreteblksource{lno, blk, ""}
 	case isnumstart(p.r.ch()):
@@ -154,7 +154,7 @@ func (p *parser) parsesource() (input specSource) {
 }
 
 func (p *parser) parseblock(name string) {
-	blk, hasinput := p.newstandaloneblk(name, &blkconstraint{})
+	blk, hasinput := p.newstandaloneblk(name, inpdef_allowed)
 	if !hasinput {
 		p.sinkNames[name] = blk
 	}
@@ -188,7 +188,7 @@ func (p *parser) parsegroup(name string) {
 		lno := p.r.sourceline()
 		var cur portMapper
 		if dollar {
-			f, _ := p.parsefactory(haveinout(""))
+			f, _ := p.parsefactory(inpdef_prohibited)
 			m := make(map[string]*Blk)
 			for _, sel := range names {
 				blk := p.newblk(lno, fmt.Sprintf("%s#%d.%s", name, idx, sel), f.typ, f.param)
@@ -197,7 +197,7 @@ func (p *parser) parsegroup(name string) {
 			}
 			cur = &dollarPortMapper{p.r.sourceline(), m}
 		} else {
-			f, _ := p.parsefactory(haveinout(names...))
+			f, _ := p.parsefactory(inpdef_prohibited)
 			blk := p.newblk(lno, fmt.Sprintf("%s#%d", name, idx), f.typ, f.param)
 			blk.oc = &outputconstraint{fmt.Sprintf("group '%s' element '%s' needs names: %v", name, f.typ, names), names}
 			cur = blk
@@ -227,7 +227,7 @@ func (p *parser) parsegroup(name string) {
 	p.sourceNames[name] = last
 }
 
-func (p *parser) parsefactory(constr *blkconstraint) (f *factory, inputs []specSource) {
+func (p *parser) parsefactory(inpdisp int) (f *factory, inputs []specSource) {
 	p.r.skiplinespace()
 	if !p.r.eatch('[') {
 		panic("invalid factory block spec")
@@ -238,17 +238,6 @@ func (p *parser) parsefactory(constr *blkconstraint) (f *factory, inputs []specS
 	f.typ, err = p.GetType(f.tname)
 	if err != nil {
 		panic(err)
-	}
-	if len(constr.mustonlyinput) != 0 {
-		if len(constr.mustonlyinput) != len(f.typ.Input()) {
-			panic(errf("type '%s' must have %d inputs, not %d",
-				f.tname, len(constr.mustonlyinput), len(f.typ.Input())))
-		}
-		for _, n := range constr.mustonlyinput {
-			if f.typ.Input().Port(n) == Invalid {
-				panic(errf("type '%s' does not have required %s", f.tname, nice(inport, n)))
-			}
-		}
 	}
 
 	for {
@@ -266,23 +255,10 @@ func (p *parser) parsefactory(constr *blkconstraint) (f *factory, inputs []specS
 			var emptyparam PosParam
 			f.param = emptyparam
 		}
-		if constr.inpdisp == inpdef_prohibited {
+		if inpdisp == inpdef_prohibited {
 			panic(errf("input declaration for '%s' not allowed", f.tname))
 		}
 		inputs = append(inputs, p.parsesource())
-	}
-
-	switch constr.inpdisp {
-	case inpdef_required:
-		if len(inputs) != len(f.typ.Input().Names()) {
-			panic(errf("input count mismatch for '%s': needed %d, have %d",
-				f.tname, len(f.typ.Input().Names()), len(inputs)))
-		}
-	case inpdef_allowed:
-		if len(inputs) != 0 && len(inputs) != len(f.typ.Input().Names()) {
-			panic(errf("input count mismatch for '%s': needs either zero or %d, have %d",
-				f.tname, len(f.typ.Input().Names()), len(inputs)))
-		}
 	}
 
 	return
@@ -317,16 +293,18 @@ func (p *parser) parseparam() Param {
 	return nil // not reached
 }
 
-func (p *parser) newstandaloneblk(name string, constr *blkconstraint) (*Blk, bool) {
+func (p *parser) newstandaloneblk(name string, inpdisp int) (*Blk, bool) {
 	lno := p.r.sourceline()
-	f, inputs := p.parsefactory(constr)
+	f, inputs := p.parsefactory(inpdisp)
 	if name == "" {
 		name = fmt.Sprintf("«%s:%d»", f.tname, lno)
 	}
 	blk := p.newblk(lno, name, f.typ, f.param)
 	if len(inputs) != 0 {
 		for i, n := range f.typ.Input().Names() {
-			p.vlink = append(p.vlink, Link{&concreteblksink{lno, blk, n}, inputs[i]})
+			if i < len(inputs) {
+				p.vlink = append(p.vlink, Link{&concreteblksink{lno, blk, n}, inputs[i]})
+			}
 		}
 	}
 	return blk, len(inputs) != 0
@@ -367,17 +345,3 @@ const (
 	inpdef_required
 	inpdef_prohibited
 )
-
-type blkconstraint struct {
-	inpdisp       int
-	mustonlyinput []string // blk must have exactly these inputs
-}
-
-var anyblock = &blkconstraint{}
-
-func haveinout(names ...string) *blkconstraint {
-	return &blkconstraint{
-		inpdisp:       inpdef_prohibited,
-		mustonlyinput: names,
-	}
-}
